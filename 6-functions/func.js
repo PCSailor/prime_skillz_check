36eb63988230b4bd63ec70fb6e39f37a5aaad7fb
PCSailor <Phil@PhilCurtis.io>
2017-02-13 14:13:36 -0600

    Update func.js

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -1,13 +1,26 @@
 // 1 - Write a function declaration
 // that takes no arguments and
 // returns the value 4
-
+function declaration () {
+  4 += 0;
+  return;
+}
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
+var a = 10;
+var b = 10;
+function declarationTwo (a,b) {
+  a * b
+  return;
+}
 
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+var functionExpression = function(a) {
+  var a = 10
+  console.log(functionExpression());
+};
